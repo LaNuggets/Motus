70a1d74d2334c0739a4121c343854ba784c220f9
Stan <stanislas.ibrahim@ynov.com>
2024-04-26 08:11:54 +0200

    modif

--- Main.java
+++ Main.java
@@ -13,15 +13,16 @@ public class Main {
         String filePath = "Motus/Mots.txt";
         String randomWord = Randomword.pickRandomWord(filePath);
 
-
         // Afficher les traits pour chaque lettre du mot
         StringBuilder trait = new StringBuilder();
         trait.append("===================\n");
         for (int i = 0; i < randomWord.length(); i++) {
             trait.append("| _ ");
         }
+
         trait.append("|\n");
         trait.append("===================");
         System.out.println("Mot à deviner : \n" + trait.toString());
+    }
     }
 }
