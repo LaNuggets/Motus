f7c9333f1e95cbb74b8601e37303cd5b5e244abb
dcamille <camilledouaud@ynov.Com>
2024-04-26 10:17:15 +0200

    orange letter

--- Game.java
+++ Game.java
@@ -60,32 +60,35 @@ public class Game {
         return trait.toString();
     }
 
-    public String letterMatch(String randomWord, String userWord) {
-        Scanner scanner = new Scanner(System.in);
-        String ANSI_GREEN = "\u001B[32m";
-        String ANSI_RED = "\u001B[31m";
-        String ANSI_RESET = "\u001B[0m";
+public String letterMatch(String randomWord, String userWord) {
+    Scanner scanner = new Scanner(System.in);
+    String ANSI_GREEN = "\u001B[32m";
+    String ANSI_RED = "\u001B[31m";
+    String ANSI_ORANGE = "\u001B[33m";
+    String ANSI_RESET = "\u001B[0m";
 
-        while (!userWord.equals(randomWord)) {
-            StringBuilder trait = new StringBuilder();
-            trait.append("===================\n");
-            for (int i = 0; i < this.wordLength; i++) {
-                char userLetter = userWord.charAt(i);
-                if (randomWord.charAt(i) == userLetter) {
-                    trait.append("| " + ANSI_GREEN + userLetter + ANSI_RESET + " "); // vrai
-                } else {
-                    trait.append("| " + ANSI_RED + userLetter + ANSI_RESET + " "); // faux
-                }
+    while (!userWord.equals(randomWord)) {
+        StringBuilder trait = new StringBuilder();
+        trait.append("===================\n");
+        for (int i = 0; i < this.wordLength; i++) {
+            char userLetter = userWord.charAt(i);
+            if (randomWord.charAt(i) == userLetter) {
+                trait.append("| " + ANSI_GREEN + userLetter + ANSI_RESET + " "); // vrai
+            } else if (randomWord.contains(String.valueOf(userLetter))) {
+                trait.append("| " + ANSI_ORANGE + userLetter + ANSI_RESET + " "); // faux mais présent
+            } else {
+                trait.append("| " + ANSI_RED + userLetter + ANSI_RESET + " "); // faux
             }
-            trait.append("|\n");
-            trait.append("===================");
-            System.out.println("Mot à deviner : \n" + trait.toString());
-
-            System.out.println("Veuillez entrer un nouveau mot :");
-            userWord = scanner.nextLine();
         }
-        return userWord;
+        trait.append("|\n");
+        trait.append("===================");
+        System.out.println("Mot à deviner : \n" + trait.toString());
+
+        System.out.println("Veuillez entrer un nouveau mot :");
+        userWord = scanner.nextLine();
     }
+    return userWord;
+}
 
 
 }
